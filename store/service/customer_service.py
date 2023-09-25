ac49eb3123dcd7056e9dd348c91d5e432a9d36af
brunoleob <brunoleo3347@gmail.com>
2023-09-25 15:42:46 -0300

    feat: api request to populate customers

--- store/service/customer_service.py
+++ store/service/customer_service.py
@@ -1,6 +1,42 @@
+import requests
+
 from store.models import Customer
 
 
 class CustomerService:
     def customer_list(self):
         return Customer.objects.all()
+
+    def fetch_and_create_customers(self):
+        # URL da API JSONPlaceholder para listar usuários
+        api_url = 'https://jsonplaceholder.typicode.com/users'
+
+        try:
+            response = requests.get(api_url)
+
+            if response.status_code == 200:
+                users = response.json()
+                for user in users:
+                    # Verifica se um usuário com o mesmo email já existe no banco de dados
+                    existing_customer = Customer.objects.filter(
+                        email=user['email']).first()
+
+                    if existing_customer:
+                        existing_customer.name = user['name']
+                        existing_customer.save()
+                    else:
+                        customer = Customer(
+                            name=user['name'],
+                            email=user['email'],
+                            phone=user['phone']
+                        )
+                        customer.save()
+
+                return True
+            else:
+                print(f'Error in API request: {response.status_code}')
+                return False
+
+        except Exception as e:
+            print(f'Error accessing the API: {str(e)}')
+            return False
